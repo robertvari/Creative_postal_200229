f7449ac5a9f6ccb9e50cb3973227800ee80be4bc
Robert Vari <mail.robertvari@gmail.com>
2020-05-02 14:54:26 +0200

    Create unique username

--- CreativePortal_API/users/models.py
+++ CreativePortal_API/users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+import time
 
 from .managers import CreativeUserManager
 
@@ -21,3 +25,14 @@ class CreativeUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+@receiver(pre_save, sender=CreativeUser)
+def create_username(sender, instance, **kwargs):
+    if not instance.username:
+        username = instance.email.split("@")[0]
+
+        if CreativeUser.objects.filter(username=username).exists():
+            username = f"{username}-{int(time.time())}"
+
+        instance.username = username
