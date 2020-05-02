bc4a8c20c746de71d9fdaa1b20001e0426a9faec
Robert Vari <mail.robertvari@gmail.com>
2020-05-02 15:57:46 +0200

    Side menu components

--- creativeportal_gui/src/component/layout/Sidebar.js
+++ creativeportal_gui/src/component/layout/Sidebar.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Logo from "../images/logo.png"
+import "./Sidebar.css"
+
+
+function MenuItem(props) {
+    return (
+        <div>
+            <i className={props.icon}/> {props.text}
+        </div>
+    )
+}
+
 
 function Sidebar(props) {
     return (
         <div>
-            <div>Creative Portal</div>
+            <div className="site_header">
+                <img src={Logo} alt=""/>
+                Creative Portal
+            </div>
+
+            <hr/>
+
+            <MenuItem text={"Home"} icon={"fas fa-home"}/>
+            <MenuItem text={"Notifications"} icon={"fas fa-bell"}/>
+            <MenuItem text={"Likes"} icon={"fas fa-thumbs-up"}/>
+            <MenuItem text={"Favorites"} icon={"fas fa-star"}/>
+            <MenuItem text={"Collections"} icon={"fas fa-object-group"}/>
 
         </div>
     );
