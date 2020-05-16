bbf66cdd183f99608cc4f080a11b2c849158ca4a
Robert Vari <mail.robertvari@gmail.com>
2020-05-16 17:19:48 +0200

    Login component with API response: token

--- creativeportal_gui/src/component/context/UserContext.js
+++ creativeportal_gui/src/component/context/UserContext.js
@@ -1,15 +1,55 @@
 import React, {createContext, useState} from "react"
-
+import axios from 'axios'
 export const UserContext = createContext(true)
 
 export const UserProvider = (props) => {
-    const [logged_in, set_logged_in] = useState(false)
+    const [logged_in, set_logged_in] = useState(null)
+    const [errors, set_errors] = useState([])
+    const API_URL = process.env.REACT_APP_API_URL
+
+    const validate_email = (email) => {
+        if(email.length === 0){
+            set_errors(["Email is required"])
+            return false
+        }
+
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            set_errors(["Email is not valid"])
+            return false
+        }
+
+        return true
+    }
+
+    const log_in_user = async (email, password) => {
+        try{
+            const res =  await axios({
+                method: "post",
+                url: `${API_URL}auth/login/`,
+                data: {
+                    email: email,
+                    password: password
+                }
+            })
+
+            set_logged_in(res.data.key)
+        }catch (e) {
+            set_errors(["Invalid credentials"])
+        }
+    }
 
     return (
         <UserContext.Provider value={
             {
                 logged_in:logged_in,
-                set_logged_in:set_logged_in
+                set_logged_in:set_logged_in,
+
+                errors: errors,
+                set_errors: set_errors,
+
+                log_in_user: log_in_user,
+                validate_email: validate_email
             }
         }>
 
--- creativeportal_gui/src/component/user/Login.js
+++ creativeportal_gui/src/component/user/Login.js
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './Login.css'
 import FormHeader from "../widgets/FormHeader";
+import {UserContext} from "../context/UserContext";
+import {useHistory} from "react-router-dom"
 
 function Login(props) {
+    const {logged_in, log_in_user, errors, set_errors, validate_email} = useContext(UserContext)
+    const [email, set_email] = useState("")
+    const [password, set_password] = useState("")
+    const history = useHistory()
+
     const sign_in_action = (e) => {
         e.preventDefault()
+
+        set_errors([])
+
+        if(!validate_email(email)){
+            return
+        }
+
+        if(password.length === 0){
+            set_errors(["Password is required"])
+            return
+        }
+
+        log_in_user(email, password)
     }
+
+    useEffect(() => {
+        if(process.env.NODE_ENV === "development"){
+            set_email("mail.pythonsuli@gmail.com")
+            set_password("testpas123")
+        }
+    }, [])
+
+    useEffect(() => {
+        if(logged_in){
+            history.push("/")
+        }
+    }, [logged_in])
 
     return (
         <div className="center_page">
             <form>
                 <FormHeader text={"Sign In"}/>
 
-                <input type="email" placeholder="email"/>
-                <input type="password" placeholder="Password"/>
+                <input type="email" placeholder="email" value={email} onChange={e => set_email(e.target.value)}/>
+                <input type="password" placeholder="Password" value={password} onChange={e => set_password(e.target.value)}/>
 
                 <hr/>
+
+                {errors &&
+                    <div className="errors">
+                        {errors.map(error => <p key={error}>{error}</p>)}
+                    </div>
+                }
+
                 <button onClick={sign_in_action}>Sign In</button>
             </form>
         </div>
