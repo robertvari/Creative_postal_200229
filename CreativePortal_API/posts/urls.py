74bd178563defc19e793cb55dfd91feb375d7857
Robert Vari <mail.robertvari@gmail.com>
2020-05-31 14:21:49 +0200

    API: Post create

--- CreativePortal_API/posts/urls.py
+++ CreativePortal_API/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, CreatePostView
 
 urlpatterns = [
-    path('', PostListView.as_view())
+    path('', PostListView.as_view()),
+    path('create/', CreatePostView.as_view())
 ]
--- CreativePortal_API/posts/views.py
+++ CreativePortal_API/posts/views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Post
@@ -9,3 +12,15 @@ class PostListView(ListAPIView):
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostItemSerializer
+
+
+class CreatePostView(APIView):
+    def post(self, request):
+        post = Post.objects.create(
+            user=request.user,
+            title=request.data["title"],
+            description=request.data["description"],
+            image=request.data["image"],
+        )
+
+        return Response(post.slug, status.HTTP_201_CREATED)
